64251e3995c779579177dfef4c6f364d8e3b2184
Brendan Graetz <bguiz@users.noreply.github.com>
2024-04-01 20:41:42 +0800

    feat: load previously created skills when subscribing to an existing topic

--- back/subscribe-to-topic.js
+++ back/subscribe-to-topic.js
@@ -3,7 +3,10 @@ const {
 } = require('@hashgraph/sdk');
 
 const { topicGet } = require('./topic-get.js');
-const { skillSubscribe } = require('../util/skill-subscribe.js');
+const {
+  skillGetAll,
+  skillSubscribe,
+} = require('../util/skill-subscribe.js');
 
 // key: topicId
 // value: TopicMessageQuery (subscription)
@@ -11,23 +14,19 @@ const subscriptions = new Map();
 
 async function getSubscription(topicId, wsServer) {
   const topicIdStr = topicId.toString();
+  const socketId = `hcs-skill-${topicIdStr}`;
+
+  // get all previously submitted skills for this topic
+  await skillGetAll(topicIdStr, onGetSkillCallback);
 
   // Re-use existing TopicMessageQuery subscription if present.
   // Otherwise initialise a new one
   let subscription = subscriptions.get(topicIdStr);
   if (!subscription) {
-    const socketId = `hcs-skill-${topicIdStr}`;
     console.log(`Initialising new TopicMessageQuery subscription for ${topicId} relayed on socket ID ${socketId}`);
 
     try {
-      subscription = await skillSubscribe(topicIdStr, function(err, obj) {
-        if (err) {
-          console.error('Verification errors');
-          console.log(err, obj);
-          return;
-        }
-        wsServer.emit(socketId, JSON.stringify(obj));
-      });
+      subscription = await skillSubscribe(topicIdStr, onGetSkillCallback);
       subscriptions.set(topicIdStr, subscription);
       console.log('SUCCESS: TopicMessageQuery()', topicIdStr, socketId);
     } catch (error) {
@@ -37,6 +36,15 @@ async function getSubscription(topicId, wsServer) {
   }
 
   return subscription;
+
+  function onGetSkillCallback(err, obj) {
+    if (err) {
+      console.error('Verification errors');
+      console.log(err, obj);
+      return;
+    }
+    wsServer.emit(socketId, JSON.stringify(obj));
+  }
 }
 
 async function subscribeToTopic(wsServer, topicIdReq) {
